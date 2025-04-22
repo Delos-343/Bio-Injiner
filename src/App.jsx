8614731923fe18561b15530faface481e514fb05
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2025-04-22 14:47:20 +0700

    Set up Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './App.css'
+import { Navbar } from './components'
 
 function App() {
   return (
     <>
-      <div className="text-6xl font-bold underline">
-        <h1> Hello World </h1>
-      </div>
+      <Navbar />
     </>
   )
 }
