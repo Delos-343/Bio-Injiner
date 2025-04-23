be9241bb9eb7011169df60f425786183a08289ed
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2025-04-23 18:24:35 +0700

    Added new themes to backend

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,11 +3,6 @@ import React from 'react'
 const Modal = () => {
   return (
     <>
-        <div className="modal modal-open">
-            <h1 className="text-2xl font-bold">
-                Modal
-            </h1>
-        </div>
     </>
   )
 }
